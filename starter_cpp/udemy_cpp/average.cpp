201654de5184c3161a0572b6e56f40a30e5f047a
Sandheep Krishna A <109921635+sandy1702@users.noreply.github.com>
2022-08-22 21:18:17 +0530

    :)

---
+++ starter_cpp/udemy_cpp/average.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int maths, physics, chemistry;
+	cin>>maths;
+	cin>>physics;
+	cin>>chemistry;
+	float average=(maths+physics+chemistry)/3.0;
+	cout<<average;
+}
