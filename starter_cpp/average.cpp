7558c89ecd27a25d08ad854eb020aa609543094e
Sandheep Krishna A <109921635+sandy1702@users.noreply.github.com>
2022-08-22 21:09:23 +0530

    :)

---
+++ starter_cpp/average.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name="sandy";
+    int mark1=80, mark2=90, average;
+    average=mark1+mark2/2;
+    cout<<"The average of two subjects is : "
+}
