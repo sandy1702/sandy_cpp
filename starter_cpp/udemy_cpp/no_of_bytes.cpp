53d5d18a2fd90bfea282dfd1cb8984ea65fe3d15
Sandheep Krishna A <109921635+sandy1702@users.noreply.github.com>
2022-08-22 21:19:47 +0530

    :)

---
+++ starter_cpp/udemy_cpp/no_of_bytes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"integer : "<<sizeof(int)<<endl;
+	cout<<"float : "<<sizeof(float)<<endl;
+	cout<<"double : "<<sizeof(double)<<endl;
+	cout<<"string : "<<sizeof(string)<<endl;
+	cout<<"character : "<<sizeof(char)<<endl; 
+	cout<<"long integer : "<<sizeof(long int)<<endl;
+	cout<<"long long integer : "<<sizeof(long long int)<<endl;
+	cout<<"short integer : "<<sizeof(short int)<<endl;
+
+}
