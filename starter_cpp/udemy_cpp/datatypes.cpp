b50f3b010cbd9871cfa3c5c50b9dc19d05ed4130
Sandheep Krishna A <109921635+sandy1702@users.noreply.github.com>
2022-08-22 21:19:05 +0530

    :)

---
+++ starter_cpp/udemy_cpp/datatypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	int num=4;
+	cout<<"integer "<<num<<endl;
+	char alpha='A';
+	cout<<"character "<<alpha<<endl;
+	string str="daddy";
+	cout<<"string "<<str<<endl;
+	float fl=1.22456787;
+	double dl=1.22456787;
+	cout<<setprecision(10)<<fl<<endl;
+	cout<<setprecision(10) <<dl<<endl;
+	
+
+}
